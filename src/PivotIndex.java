fcb00a980d1e6f2169de3890a489e2b55d830e12
root <jayasinghe.ashen@gmail.com>
2022-12-31 20:39:26 +0530

    array problem solving recap

---
+++ src/PivotIndex.java
@@ -0,0 +1,40 @@
+
+/*
+Example 1:
+
+Input: nums = [1,7,3,6,5,6]
+Output: 3
+Explanation:
+The pivot index is 3.
+Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+Right sum = nums[4] + nums[5] = 5 + 6 = 11
+*/
+public class PivotIndex {
+
+
+    public static int pivotIndex(int[] nums) {
+        //sum of all elements in index
+        int n = nums.length;
+        int sum = 0;
+        for(int i=0; i < n; i++){
+            sum += nums[i];
+        }
+        int leftSum =0;
+        for(int i=0; i < n; i++){
+            /*If the leftSum is equal to the sum of all elements except the current element,
+            then the index of the current element is returned,
+            as that is the index of the element that makes the sum of all elements equal to 0.*/
+            if (leftSum == sum - leftSum - nums[i]) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {1,7,3,6,5,6};
+        int result = pivotIndex(nums);
+    }
+}
---
+++ src/RunningSumOf1DArray.java
@@ -0,0 +1,32 @@
+
+/*
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+*/
+public class RunningSumOf1DArray {
+
+
+    /*Time Complexity: O(N) where N is the length of nums
+Space Complexity: O(N) for our running sum array
+or O(1) with an in-place approach
+    * */
+    public static int[] runningSum(int[] nums) { //nums: [1,2,3,4]
+        int[] ans = new int[nums.length]; //ans: [0,0,0,0]
+        ans[0] = nums[0];                //ans: [1,3,0,0]
+        for(int i=1; i<nums.length; i++){  //i:1 , nums: [2,3,4]         i:2, nums[3,4]
+            ans[i] = ans[i-1] + nums[i];  //1st -> ans[1] = ans[0] + nums[1],  (1 + 2) | 2nd -> ans[1] + nums[2] (3 + 3)
+        }
+        return ans;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {1,2,3,4};
+        int[] result = runningSum(nums);
+    }
+}
